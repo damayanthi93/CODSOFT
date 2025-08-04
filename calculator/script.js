081938a0e1055e6805bcb0d1d8101465d02e46cb
damayanthi93 <damayantivelamala@gmail.com>
2025-08-04 21:01:12 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  if (display.innerText === "0") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  if (display.innerText.length === 1 || display.innerText === "Error") {
+    display.innerText = "0";
+  } else {
+    display.innerText = display.innerText.slice(0, -1);
+  }
+}
+
+function calculateResult() {
+  try {
+    let result = eval(display.innerText);
+    display.innerText = result;
+  } catch (e) {
+    display.innerText = "Error";
+  }
+}
